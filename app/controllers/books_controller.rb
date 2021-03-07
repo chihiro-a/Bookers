b04ea2d4015c1a18b7601939d933b1da6f65e656
EC2 Default User <ec2-user@ip-172-31-44-125.ec2.internal>
2021-03-07 12:19:28 +0000

    indexページform_with編集途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,23 @@ class BooksController < ApplicationController
   def show
   end
 
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_id)
+    book.save
+    redirect_to books_path
+    # リダイレクト先確認
+  end
+
   def edit
+  end
+
+
+  private
+  def book_id
+    params.require(:book).permit( :title, :body)
   end
 end
