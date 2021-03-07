863e069d5fba210554a997b4c18626862106d951
EC2 Default User <ec2-user@ip-172-31-44-125.ec2.internal>
2021-03-07 08:28:59 +0000

    ルーティングの記述変更まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  get 'homes/top'
+  get 'books/index' => 'books#index'
+  get 'books/show' => 'books#show'
+  get 'books/edit' => 'books#edit'
+  get 'homes/top' => 'homes#top'
+  # ここあとでルートURLに変更する…'homes/top'を'/'にすればおけ？
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
