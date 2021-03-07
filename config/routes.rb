18d72c6cc18248c7490b9ca511ce7037c8c2104f
EC2 Default User <ec2-user@ip-172-31-44-125.ec2.internal>
2021-03-07 11:42:20 +0000

    ルーチィングの修正、Topページのレイアウト作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get 'homes/top' => 'homes#top'
   # ここあとでルートURLに変更する…'homes/top'を'/'にすればおけ？
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :blogs
 end
