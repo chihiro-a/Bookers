9302dd1799da5c87258f9c6dbdf340de1093c7c1
EC2 Default User <ec2-user@ip-172-31-44-125.ec2.internal>
2021-03-13 09:10:48 +0000

    フラッシュメッセージ実装完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,32 +1,46 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
+    # index内で空の@bookを作成することができる！
+    @books = Book.all
   end
 
   def show
     @book = Book.find(params[:id])
   end
 
-  def new
-    @book = Book.new
-    # newの中でインデックスへアクセスするよう記載できないか？？
-  end
 
   def create
     # @book = Book.new
     # new内での定義ではなくcreate内で定義した
     @book = Book.new(book_params)
     @book.save
-    redirect_to ("/books/#{@book[:id]}")
+    redirect_to ("/books/#{@book[:id]}"),notice: 'Book was successfully created.'
+
     # リダイレクト先確認　showへ
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to ("/books/#{book[:id]}"),notice: 'Book was successfully updated.'
+    # Showへリダイレクト
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to("/books")
   end
 
 
   private
   def book_params
-    params.permit(:title, :body)
-    # require(:book).の記述を抜いた
+    params.require(:book).permit(:title, :body)
+    # require(:book).の記述を抜くと編集ができない。入れると新規投稿ができない。
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 
   get '/' => 'homes#top'
-  get 'books' => 'books#index'
+  get 'books' =>  'books#index'
   # booksをgetしたときbooks#newも一緒にしたい…newにindexへのリダイレクトを含めてみる…
   get 'books/:id' => 'books#show'
   get 'books/:id/edit' => 'books#edit'
   post 'books' => 'books#create'
+  patch 'books/:id' => 'books#update'
+  delete 'books/:id' => 'books#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
